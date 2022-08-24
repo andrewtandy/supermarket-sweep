8ba238b239785a2370209d7f0146d62256788bf0
Andrew Tandy <andrewtandy3@gmail.com>
2022-08-24 23:35:04 +0100

    Uploading testing files for pptr

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# supermarket-sweep
+# supermarket-sweep // supermarket-screep
 
 ## Personal project to help in these tough times
 Andrew Tandy 2022
--- src/main.js
+++ src/main.js
@@ -4,4 +4,19 @@ import './registerServiceWorker'
 import router from './router'
 import store from './store'
 
+import firebase from "firebase"
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCDuVTPzQy0YkOXvIAe6WCoFxec8jZMsiQ",
+    authDomain: "supermarket-screep.firebaseapp.com",
+    projectId: "supermarket-screep",
+    storageBucket: "supermarket-screep.appspot.com",
+    messagingSenderId: "146405965707",
+    appId: "1:146405965707:web:8114dcb5d05a7c0d62398e"
+  };
+
+// Initialise Firebase
+firebase.initializeApp(firebaseConfig);
+
 createApp(App).use(store).use(router).mount('#app')
